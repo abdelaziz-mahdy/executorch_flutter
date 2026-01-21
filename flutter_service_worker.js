0a11bc943504229aef4885ed74b707542eabf9e1
abdelaziz-mahdy <abdelaziz-mahdy@users.noreply.github.com>
2026-01-21 00:08:07 +0000

    deploy: 11a4b064264676553c686263750941eb81ce9c86

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a368e150e1f00893b39f9d799b4eda29",
+const RESOURCES = {"flutter_bootstrap.js": "398ea6fc587fd02977547f8ff9dad7be",
 "version.json": "fb53a009187e6eb42e4a85b40eba6d28",
 "index.html": "54bb9e92ad792992867548cd3972f5f2",
 "/": "54bb9e92ad792992867548cd3972f5f2",
